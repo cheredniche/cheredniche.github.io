1cfcd26f854002f15fe584d5d30787d1a5eb9351
peace_akame <blackstile2014@gmail.com>
2022-09-14 12:27:23 +0300

    .js test

--- static/js/main.js
+++ static/js/main.js
@@ -1,77 +1,151 @@
+/*! Grained.js
+* Author : Sarath Saleem  - https://github.com/sarathsaleem
+* MIT license: http://opensource.org/licenses/MIT
+* GitHub : https://github.com/sarathsaleem/grained
+* v0.0.1
+*/
+(function (window, doc) {
 
-(function ($) {
     "use strict";
 
-    /*==================================================================
-    [ Validate after type ]*/
-    $('.validate-input .input100').each(function(){
-        $(this).on('blur', function(){
-            if(validate(this) == false){
-                showValidate(this);
-            }
-            else {
-                $(this).parent().addClass('true-validate');
-            }
-        })    
-    })
-  
-  
-    /*==================================================================
-    [ Validate ]*/
-    var input = $('.validate-input .input100');
+    function grained(ele, opt) {
 
-    $('.validate-form').on('submit',function(){
-        var check = true;
+        var element = null,
+            elementId = null,
+            selectorElement = null;
 
-        for(var i=0; i<input.length; i++) {
-            if(validate(input[i]) == false){
-                showValidate(input[i]);
-                check=false;
-            }
+        if (typeof ele === 'string') {
+            element = doc.getElementById(ele.split('#')[1]);
         }
 
-        return check;
-    });
+        if (!element) {
+            console.error('Grained: cannot find the element with id ' + ele);
+            return;
+        } else {
+            elementId = element.id;
+        }
 
+        //set style for parent
+        if (element.style.position !== 'absolute') {
+            element.style.position = 'relative';
+        }
+        element.style.overflow = 'hidden';
 
-    $('.validate-form .input100').each(function(){
-        $(this).focus(function(){
-           hideValidate(this);
-           $(this).parent().removeClass('true-validate');
+        var prefixes = ["", "-moz-", "-o-animation-", "-webkit-", "-ms-"];
+
+        //default option values
+        var options = {
+  "animate": true,
+  "patternWidth": 328.79,
+  "patternHeight": 500,
+  "grainOpacity": 0.11,
+  "grainDensity": 7.35,
+  "grainWidth": 1,
+  "grainHeight": 5.96
+}
+grained("#elementId", options);
+
+/*
+background-color :rgb(205, 179, 213)
+*/
+        };
+
+        Object.keys(opt).forEach(function (key) {
+            options[key] = opt[key];
         });
-    });
 
-     function validate (input) {
-        if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
-            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
-                return false;
+
+        var generateNoise = function () {
+            var canvas = doc.createElement('canvas');
+            var ctx = canvas.getContext('2d');
+            canvas.width = options.patternWidth;
+            canvas.height = options.patternHeight;
+            for (var w = 0; w < options.patternWidth; w += options.grainDensity) {
+                for (var h = 0; h < options.patternHeight; h += options.grainDensity) {
+                    var rgb = Math.random() * 256 | 0;
+                    ctx.fillStyle = 'rgba(' + [rgb, rgb, rgb, options.grainOpacity].join() + ')';
+                    ctx.fillRect(w, h, options.grainWidth, options.grainHeight);
+                }
+            }
+            return canvas.toDataURL('image/png');
+        };
+
+        function addCSSRule(sheet, selector, rules, index) {
+            var ins = '';
+            if (selector.length) {
+                ins = selector + "{" + rules + "}";
+            } else {
+                ins = rules;
+            }
+
+            if ("insertRule" in sheet) {
+                sheet.insertRule(ins, index);
+            } else if ("addRule" in sheet) {
+                sheet.addRule(selector, rules, index);
             }
         }
-        else {
-            if($(input).val().trim() == ''){
-                return false;
+
+
+        var noise = generateNoise();
+
+        var animation = '',
+            keyFrames = ['0%:-10%,10%', '10%:-25%,0%', '20%:-30%,10%', '30%:-30%,30%', '40%::-20%,20%', '50%:-15%,10%', '60%:-20%,20%', '70%:-5%,20%', '80%:-25%,5%', '90%:-30%,25%', '100%:-10%,10%'];
+
+        var pre = prefixes.length;
+        while (pre--) {
+            animation += '@' + prefixes[pre] + 'keyframes grained{';
+            for (var key = 0; key < keyFrames.length; key++) {
+                var keyVal = keyFrames[key].split(':');
+                animation += keyVal[0] + '{';
+                animation += prefixes[pre] + 'transform:translate(' + keyVal[1] + ');';
+                animation += '}';
+            }
+            animation += '}';
+        }
+
+        //add animation keyframe
+        var animationAdded = doc.getElementById('grained-animation');
+        if (animationAdded) {
+            animationAdded.parentElement.removeChild(animationAdded);
+        }
+        var style = doc.createElement("style");
+        style.type = "text/css";
+        style.id = 'grained-animation';
+        style.innerHTML = animation;
+        doc.body.appendChild(style);
+
+        //add custimozed style
+        var styleAdded = doc.getElementById('grained-animation-' + elementId);
+        if (styleAdded) {
+            styleAdded.parentElement.removeChild(styleAdded);
+        }
+
+        style = doc.createElement("style");
+        style.type = "text/css";
+        style.id = 'grained-animation-' + elementId;
+        doc.body.appendChild(style);
+
+        var rule = 'background-image: url(' + noise + ');';
+        rule += 'position: absolute;content: "";height: 300%;width: 300%;left: -100%;top: -100%;';
+        pre = prefixes.length;
+        if (options.animate) {
+            while (pre--) {
+                rule += prefixes[pre] + 'animation-name:grained;';
+                rule += prefixes[pre] + 'animation-iteration-count: infinite;';
+                rule += prefixes[pre] + 'animation-duration: ' + options.grainChaos + 's;';
+                rule += prefixes[pre] + 'animation-timing-function: steps(' +options.grainSpeed + ', end);';
             }
         }
+
+        //selecter element to add grains
+        selectorElement = '#' + elementId + '::before';
+
+
+        addCSSRule(style.sheet, selectorElement, rule);
+
+
     }
 
-    function showValidate(input) {
-        var thisAlert = $(input).parent();
-
-        $(thisAlert).addClass('alert-validate');
-
-        $(thisAlert).append('<span class="btn-hide-validate">&#xf136;</span>')
-        $('.btn-hide-validate').each(function(){
-            $(this).on('click',function(){
-               hideValidate(this);
-            });
-        });
-    }
-
-    function hideValidate(input) {
-        var thisAlert = $(input).parent();
-        $(thisAlert).removeClass('alert-validate');
-        $(thisAlert).find('.btn-hide-validate').remove();
-    }
-    
-
-})(jQuery);
+    window.grained = grained;
+    //END
+})(window, document);
